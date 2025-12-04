21568ac4a916cf65fda3030a418befb7706521b1
victor <victorsancheznogueira@gmail.com>
2025-12-04 08:34:00 +0100

    fix: articulo route

--- src/server/articulosRoutes.js
+++ src/server/articulosRoutes.js
@@ -2,7 +2,7 @@
 import express from "express";
 import multer from "multer";
 import path from "path";
-import Articulo from "../modelos/Articulo.js";
+import Articulo from "../model/Articulo.js";
 import { fileURLToPath } from "url";
 import fs from 'fs';
 
