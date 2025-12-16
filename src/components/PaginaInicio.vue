0f1027486cddac0f30acabc86ca7c37dcf0aeca8
victor <victorsancheznogueira@gmail.com>
2025-12-16 08:39:00 +0100

    move caht to pagina inicio

--- src/components/PaginaInicio.vue
+++ src/components/PaginaInicio.vue
@@ -27,6 +27,16 @@ onMounted(() => {
         showContent.value = false
     }, 1000) // Oculta el contenido tras 1 segundo
 })
+
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/69410a3ab562b2197fa5ad70/1jcj0vts9';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
 </script>
 
 <style scoped>
